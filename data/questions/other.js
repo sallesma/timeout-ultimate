5b5fbd6b8a626f389bb9285ab3a8bc5fdbf07d35
Martin Salles <salles.martin@gmail.com>
2022-01-29 09:08:50 +0100

    Add question

--- data/questions/other.js
+++ data/questions/other.js
@@ -13,7 +13,20 @@ export default [
     },
     correctAnswers: ['b'],
     explanation: "Voir règle 2.7",
-    source: "Disque Tu Sais - Pablo",
+    source: "Martin",
+    level: Levels.MIDDLE,
+  },
+  {
+    category: Categories.FIELD,
+    question: "Je suis en défense, un attaquant vient se placer à moins de 3 mètres du porteur de disque. J'ai le droit de venir gêner le lanceur dans cette zone de 3 mètres.",
+    image: undefined,
+    answerChoices: {
+        a: "Vrai",
+        b: "Faux, c'est un double team",
+    },
+    correctAnswers: ['b'],
+    explanation: "Si je viens pour gêner le lanceur, c'est un double team. Je peux venir dans cette zone seulement si je suis au marquage d'un autre attaquant, c'est à dire si je suis à moins de 3 mètres de lui et que je réagis à cet autre attaquant (Règle 18.1.1.5 et Annotation 18.4)",
+    source: "Martin",
     level: Levels.MIDDLE,
   },
 ];
