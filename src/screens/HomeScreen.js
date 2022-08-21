0ad4adec50d0401ae5e68c9e4b67926acbffd572
Martin Salles <salles.martin@gmail.com>
2022-08-21 22:56:41 +0200

    Introduce i18n

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,6 +8,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import theme from '../utils/theme.js';
 import questions from '../../data/questions';
 import logo from '../../assets/icon.png';
+import I18n from '../utils/i18n';
 
 export default (props) => {
   return (
@@ -15,7 +16,7 @@ export default (props) => {
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={logo} />
       </View>
-      <Text style={styles.title}>Timeout Ultimate</Text>
+      <Text style={styles.title}>{I18n.t('homePage.appName')}</Text>
       <Text style={styles.welcomeText}>
         <Text style={styles.emphasis}>{questions.length}</Text> questions t'attendent pour apprendre les règles de
         l'ultimate !
---
+++ src/utils/locales/fr.js
@@ -0,0 +1,37 @@
+import { Levels, Categories } from '../config';
+
+export default {
+  config: {
+    levels: {
+      [Levels.EASY]: 'Facile',
+      [Levels.MIDDLE]: 'Moyen',
+      [Levels.DIFFICULT]: 'Difficile',
+      [Levels.ANY]: 'Inconnu',
+    },
+    Categories: {
+      [Categories.SPIRIT]: 'Esprit du jeu',
+      [Categories.FIELD]: 'Aire de jeu',
+      [Categories.TEAMS]: 'Les Équipes',
+      [Categories.PULL]: "L'Engagement",
+      [Categories.STATUS]: 'Le statut du disque',
+      [Categories.COUNT]: 'Le Compte',
+      [Categories.CHECK]: 'Le Check',
+      [Categories.OUT]: 'Hors des limites',
+      [Categories.RECEIVER]: 'Réceptionneur et positionnement',
+      [Categories.TURNOVERS]: 'Turnovers',
+      [Categories.GOAL]: 'Marquer un point',
+      [Categories.FOULS_INFRACTIONS_VIOLATIONS]: 'Fautes, infractions, violations',
+      [Categories.CONTINUATION]: 'Continuité après un appel de faute ou violation',
+      [Categories.FOULS]: 'Fautes',
+      [Categories.INFRACTIONS_VIOLATIONS]: 'Infractions et violations',
+      [Categories.STOPPAGE]: 'Stoppage',
+      [Categories.TIMEOUT]: 'Temps-morts',
+      [Categories.APPENDIX]: 'Appendice',
+      [Categories.HAND_SIGNALS]: 'Gestes',
+      [Categories.INDOOR]: 'Indoor',
+    },
+  },
+  homePage: {
+    appName: 'Timeout Ultimate',
+  },
+};
