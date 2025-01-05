d8f9417c4bb50ce572bb64da91bf853cb728d5fa
Martin Salles <salles.martin@gmail.com>
2025-01-05 16:02:43 +0100

    Upgrade expo to 52

--- setupJest.js
+++ setupJest.js
@@ -2,7 +2,7 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/src/private/animated/NativeAnimatedHelper.js');
 
 // Font issue with expo 48: https://github.com/expo/expo/issues/21434
 jest.mock('expo-font');
